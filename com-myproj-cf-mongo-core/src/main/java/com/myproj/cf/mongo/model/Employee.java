bf605c9446653a390e7081475476db2de034f55d
Dibyojyoti Sanyal <Dibyojyoti@users.noreply.github.com>
2023-04-14 22:54:07 +0200

    Create Employee.java

---
+++ com-myproj-cf-mongo-core/src/main/java/com/myproj/cf/mongo/model/Employee.java
@@ -0,0 +1,11 @@
+package com.myproj.cf.mongo.model;
+
+
+public class  Employee {
+  private String name;
+  private String age;
+  public  Employee ( String name, String age) {
+    this.name = name;
+    this.age = age;
+  }
+  //add getters
