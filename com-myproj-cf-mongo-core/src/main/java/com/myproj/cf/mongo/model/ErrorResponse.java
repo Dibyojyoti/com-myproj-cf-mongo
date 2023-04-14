cb43e72eb0b9703a406f46fa2d7dcd074adc8def
Dibyojyoti Sanyal <Dibyojyoti@users.noreply.github.com>
2023-04-14 22:50:00 +0200

    Update ErrorResponse.java

--- com-myproj-cf-mongo-core/src/main/java/com/myproj/cf/mongo/model/ErrorResponse.java
+++ com-myproj-cf-mongo-core/src/main/java/com/myproj/cf/mongo/model/ErrorResponse.java
@@ -1,1 +1,9 @@
-xx
+package com.myproj.cf.pgsql.model;
+
+
+public class  ErrorResponse {
+  private String message;
+  public  ErrorResponse(){}
+  public String getMessage() {return message};
+  public void setMessage(String msg){message = msg};
+}
